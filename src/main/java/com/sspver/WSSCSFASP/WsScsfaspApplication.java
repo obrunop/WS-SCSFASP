5d693f3ca57ea9021697e1578d59dc617378c3dd
Oscar Alberto Bruno Pérez <oabruno@veracruz.gob.mx>
2024-03-07 16:35:09 -0600

    Add: Creación de archivo index para mostrar en la ruta base del WS.

--- src/main/java/com/sspver/WSSCSFASP/WsScsfaspApplication.java
+++ src/main/java/com/sspver/WSSCSFASP/WsScsfaspApplication.java
@@ -2,9 +2,8 @@ package com.sspver.WSSCSFASP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class WsScsfaspApplication {
@@ -13,18 +12,12 @@ public class WsScsfaspApplication {
 		SpringApplication.run(WsScsfaspApplication.class, args);
 	}
 
-	@RestController
+	@Controller
 	public static class Api {
-		private final JdbcTemplate jdbcTemplate;
 
-		public Api(JdbcTemplate jdbcTemplate) {
-			this.jdbcTemplate = jdbcTemplate;
-		}
-
-		@GetMapping
-		public String time() {
-			final String namestamp = jdbcTemplate.queryForObject("select DB_NAME() AS myDb", String.class);
-			return "Successful Connection to " + namestamp + " Database.";
+		@GetMapping("/")
+		public String index() {
+			return "index.html";
 		}
 	}
 
