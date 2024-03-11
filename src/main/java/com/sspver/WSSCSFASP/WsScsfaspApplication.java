6b028033d57fdc4bbbaedfe6290f1f36373c2075
Oscar Alberto Bruno Pérez <oabruno@veracruz.gob.mx>
2024-03-11 14:39:06 -0600

    Refac: Cambio de método para mostrar ruta principal (/).

--- src/main/java/com/sspver/WSSCSFASP/WsScsfaspApplication.java
+++ src/main/java/com/sspver/WSSCSFASP/WsScsfaspApplication.java
@@ -2,6 +2,9 @@ package com.sspver.WSSCSFASP;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.server.WebServerFactoryCustomizer;
+import org.springframework.boot.web.servlet.server.ConfigurableServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -12,13 +15,18 @@ public class WsScsfaspApplication {
 		SpringApplication.run(WsScsfaspApplication.class, args);
 	}
 
-	@Controller
-	public static class Api {
+//	@Controller
+//	public static class Api {
+//
+//		@GetMapping("/")
+//		public String index() {
+//			return "index.html";
+//		}
+//	}
 
-		@GetMapping("/")
-		public String index() {
-			return "index.html";
-		}
+	@Bean
+	public WebServerFactoryCustomizer<ConfigurableServletWebServerFactory> webServerFactoryCustomizer() {
+		return factory -> factory.setContextPath("/");
 	}
 
 }
